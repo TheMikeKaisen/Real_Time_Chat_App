9a3ead0107e2f014e373a03c8cbb686a68c74ffb
Karthik H <h.karthiknair@gmail.com>
2025-09-26 22:38:11 +0530

    ADD: call startOtpConsumer

--- backend/mail/dist/index.js
+++ backend/mail/dist/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import { startOtpConsumer } from "./consumer.js";
 const app = express();
 dotenv.config();
+startOtpConsumer();
 app.listen(process.env.PORT, () => {
     console.log(`Server running on PORT: ${process.env.PORT}`);
 });
--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import { startOtpConsumer } from "./consumer.js";
 
 const app = express();
 
 dotenv.config();
 
+startOtpConsumer();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on PORT: ${process.env.PORT}`)
