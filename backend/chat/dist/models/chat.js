e2bf7405b2e87219e12eba47f73ed54d01914578
Karthik H <h.karthiknair@gmail.com>
2025-09-29 14:16:43 +0530

    ADD: Chat service + chat model

---
+++ backend/chat/dist/models/chat.js
@@ -0,0 +1,16 @@
+import mongoose, { Document, model, Model, Schema } from "mongoose";
+const schema = new Schema({
+    users: [
+        {
+            type: String,
+            required: true,
+        }
+    ],
+    latestMessage: {
+        text: String,
+        sender: String,
+    },
+}, {
+    timestamps: true
+});
+export const Chat = model("Chat", schema);
---
+++ backend/chat/src/models/chat.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, model, Model, Schema } from "mongoose";
+
+export interface IChat extends Document{
+    users: string[];
+    latestMessage: {
+        text: string;
+        sender: string;
+    }
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const schema: Schema<IChat> = new Schema({
+    users: [
+        {
+            type: String, 
+            required: true,
+            
+        }
+    ],
+    latestMessage: {
+                text: String,
+                sender: String,
+    },
+}, {
+    timestamps: true
+})
+
+export const Chat = model<IChat>("Chat", schema);
