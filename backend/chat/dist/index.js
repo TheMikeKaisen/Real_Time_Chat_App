bd33cc5062ea6cc8efa0df4404791f83b001e693
Karthik H <h.karthiknair@gmail.com>
2025-09-29 18:13:46 +0530

    ADD: db connection - Chat service

--- backend/chat/dist/index.js
+++ backend/chat/dist/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import chatRouter from "./routes/chat.routes.js";
 const app = express();
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectDb();
+app.use("/api/v1/chat", chatRouter);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,10 +1,19 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/db.js";
+import chatRouter from "./routes/chat.routes.js"
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+connectDb();
+
+app.use("/api/v1/chat", chatRouter);
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
+
